a75fe7aa1ca72caf5780a1fbf06d80ae34d6d434
SAMUEL <sfverneck@gmail.com>
2017-10-05 23:25:46 -0300

    delete

--- php/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt install php php-pecl-http php-dev php-mysql mysql-server php-curl php-cgi php-cli php-mbstring php-mcrypt mysql-workbench nginx
-
-curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
-
-composer global require "laravel/installer"
