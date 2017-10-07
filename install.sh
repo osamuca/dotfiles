02ae0b317f0e927612d46168ccaf29779e99d6c8
SAMUEL <sfverneck@gmail.com>
2017-10-06 21:17:11 -0300

    fix(install.sh): script de instalação.

---
+++ install.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Vim e thema onedark
+#
+#
+sudo apt-get update
+sudo apt-get install vim
+mkdir ~/.vim
+mkdir ~/.vim/colors
+mv Arquivos_Configuracao/onedark.vim ~/.vim/colors
+sudo bash -c 'echo "" >> /etc/vim/vimrc'
+sudo bash -c 'echo "colorscheme onedark" >> /etc/vim/vimrc'
+#
+
+
+
+#  Instalação do Java
+#
+#  	JDK e JRE do Java.
+#
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get update
+sudo apt-get install oracle-java8-installer -y
+sudo apt-get install oracle-java8-set-default -y
+#
+
+
+
+#  Instalação do git
+#
+#  	Controle de versão de códigos.
+#
+sudo apt-get install git -y
+#
+
+
+
+#  Instalação do zsh
+#
+#  	zsh.
+#
+sudo apt-get install zsh -y
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+#
+
+
+
+#  Instalação do VLC
+#
+#  	Reprodutor de áudio e vídeos.
+#
+sudo add-apt-repository ppa:videolan/stable-daily -y
+sudo apt-get update
+sudo apt-get install vlc -y
+#
+
+
+
+#  Instalação do tree
+#
+#  	Ao escrever tree no terminal, é mostrado uma árvore de diretórios a partir do diretório atual que esteja.
+#
+sudo apt-get install tree -y
+#
+
+
+
+#  Instalação do htop
+#
+#  	Esse programa é uma melhoria do top.
+#
+sudo apt-get install htop -y
+#
